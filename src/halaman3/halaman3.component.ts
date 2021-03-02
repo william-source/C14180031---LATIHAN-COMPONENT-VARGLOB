51762354f9acd698bf7c5de7bd3d896de837543a
william-source <c14180031@john.petra.ac.id>
2021-03-02 19:34:07 +0700

    2 files modified

--- src/halaman3/halaman3.component.ts
+++ src/halaman3/halaman3.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-halaman3',
-  templateUrl: './halaman3.component.html',
-  styleUrls: ['./halaman3.component.css']
+  selector: "app-halaman3",
+  templateUrl: "./halaman3.component.html",
+  styleUrls: ["./halaman3.component.css"]
 })
 export class Halaman3Component implements OnInit {
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    public variableglobal: DataglobalService
+  ) {
+    this.datadariglob = this.variableglobal.getData();
+  }
+  datadariglob: any;
 
   ngOnInit() {
+    this.datadariglob = this.variableglobal.getData();
   }
-
 }
